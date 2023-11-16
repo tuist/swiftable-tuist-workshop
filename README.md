87bce844f467b428eac8110771cd4ae5a9698392
Pedro <pedro@pepicrft.me>
2023-11-16 12:15:41 +0100

    Project creation

--- README.md
+++ README.md
@@ -4,6 +4,16 @@ In this workshop, we will explore [Tuist](https://tuist.io) by creating a projec
 
 The workshop is structured around a series of topics that are presented and should be followed sequentially. If, for any reason, you find yourself stuck in one of the topics, you will discover a commit SHA at the end of the topic that you can use to continue with the upcoming topics.
 
+## Assert the successful completion of a topic
+
+To assert the successful completion of a topic,
+you can run the following command passing the topic number that you just completed
+
+```bash
+# Confirming the completion of step 1
+bash <(curl -sSL https://raw.githubusercontent.com/tuist/swiftable-tuist-workshop/main/test.sh) 1
+```
+
 ## Requirements
 
 - Xcode 15
@@ -12,13 +22,14 @@ The workshop is structured around a series of topics that are presented and shou
 ## Topics
 
 1. [What is Tuist?](#1-what-is-tuist)
-2. Project creation
-3. Multi-project workspace
-4. Declaring dependencies
-5. The project graph
-6. Focused projects
-7. Caching
-7. Incremental test execution
+2. [Project creation](#2-project-creation)
+3. [Project edition](#3-project-edition)
+4. Multi-project workspace
+5. Declaring dependencies
+6. The project graph
+7. Focused projects
+8. Caching
+9. Incremental test execution
 
 ## 1. What is Tuist?
 
@@ -44,3 +55,41 @@ Tuist/
     Config.swift
 Project.swift
 ```
+
+### Install Tuist
+
+You can install Tuist by running the following command:
+
+```bash
+curl -Ls https://install.tuist.io | bash
+```
+
+## 2. Project creation
+
+Tuist provides a command for creating projects,
+`tuist init`,
+but we are going to create the project manually to familiarize ourselves more deeply with the workflows and building blocks.
+
+First of all, let's create a directory and call it `Swiftable`. Create it in this repository's directory:
+
+```bash
+mkdir -p Swiftable
+cd Swiftable
+```
+
+Then we are going to create the following directories and files:
+
+```bash
+touch Project.swift
+mkdir Tuist
+echo 'import ProjectDescription
+let config = Config()' > Tuist/Config.swift
+```
+
+### Before continuing
+
+```bash
+bash <(curl -sSL https://raw.githubusercontent.com/tuist/swiftable-tuist-workshop/main/test.sh) 2
+```
+
+If you get stuck, clone this repo and run `git checkout 2`.
---
+++ assert.sh
@@ -0,0 +1,111 @@
+#!/usr/bin/env bash
+
+step_number=$1
+
+ROOT_DIR=$(dirname "$(readlink -f "$0")")
+RESET="\033[0m"
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+
+PROJECT_DIR="."
+
+# Check if --path option is provided
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --path)
+      shift
+      if [ -d "$1" ]; then
+        PROJECT_DIR="$1"
+      else
+        echo_error "The specified directory '$1' does not exist."
+        exit 1
+      fi
+      shift
+      ;;
+    *)
+      shift
+      ;;
+  esac
+done
+
+if [ -d "$ROOT_DIR/Swiftable" ]; then
+  PROJECT_DIR="$ROOT_DIR/Swiftable"
+fi
+
+function step1() {
+    # We check that "tuist" is successfully installed
+    tuist_env_output=$(tuist version)
+
+    # Check the exit code of the command
+    if [ $? -eq 0 ]; then
+
+    if [[ $tuist_env_output =~ [0-9]+\.[0-9]+\.[0-9]+ ]]; then
+        echo_success
+    else
+        echo_error "'tuist env' did not succeed. Have you installed Tuist?"
+        exit 1
+    fi
+    else
+    echo_error "'tuist version' command failed."
+    exit 1
+    fi
+}
+
+function step2() {
+    if [ ! -f "$PROJECT_DIR/Project.swift" ]; then
+    echo_error "$PROJECT_DIR/Project.swift file does not exist."
+    exit 1
+    fi
+
+    # Check if Tuist directory exists
+    if [ ! -d "$PROJECT_DIR/Tuist" ]; then
+    echo_error "$PROJECT_DIR/Tuist directory does not exist."
+    exit 1
+    fi
+
+    # Check if Tuist/Config.swift file exists
+    if [ ! -f "$PROJECT_DIR/Tuist/Config.swift" ]; then
+    echo_error "$PROJECT_DIR/Tuist/Config.swift file does not exist."
+    exit 1
+    fi
+
+    # Check if the Tuist/Config.swift file contains the desired line
+    if ! grep -q "let config = Config()" "$PROJECT_DIR/Tuist/Config.swift"; then
+    echo_error "The $PROJECT_DIR/Tuist/Config.swift file does not contain the required line."
+    exit 1
+    fi
+
+    echo_success
+}
+
+function step3() {
+  echo_error "Running Step 3"
+  # Add your code for step 3 here
+}
+
+function echo_success() {
+  local message="$1"
+  echo -e "${GREEN}"All checks passed successfully."${RESET}"
+}
+
+function echo_error() {
+  local message="$1"
+  echo -e "${RED}Error: ${message}${RESET}"
+}
+
+# Use a case statement to determine which function to call
+case $step_number in
+  1)
+    step1
+    ;;
+  2)
+    step2
+    ;;
+  3)
+    step3
+    ;;
+  *)
+    echo "Invalid topic number. Please provide a valid step number (1, 2, or 3)."
+    exit 1
+    ;;
+esac
